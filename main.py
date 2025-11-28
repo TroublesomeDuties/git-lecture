2d77e1fd93b4a56ebe432c70f9db29fae54c5c9f
inspire12 <ox4443@naver.com>
2025-11-28 14:32:58 +0900

    Init commit
    - add gitignore
    - add main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+
+def init_git():
+    print('git init')
+
+def add_to_stage(file_path):
+    print(f'git add {file_path}')
+
+def add_all_commit():
+    print('git commit')
+
+def add_all_commit_message_option(message):
+    print(f'git commit -m {message}')
+
+def add_ignore():
+    add_to_stage('.gitignore')
+    add_all_commit_message_option('feat: add .gitignore')
+
+def add_to_stage_with_patch():
+    print('git add -p')
+
+def add_all_to_stage():
+    print('git add .')
+
+
+
+
+
+if __name__ == '__main__':
+    print('Hello Git!')
+    init_git()
+    add_to_stage('git-step.py')
+    add_ignore()
