306da3f1768e9c982ba606e5441d2a1beaefaf53
inspire12 <ox4443@naver.com>
2025-11-28 16:13:18 +0900

    feat: external api

--- embedding.py
+++ embedding.py
@@ -3,6 +3,9 @@ def embedding():
     print("call gpt")
     print("save embedding")
 
+def embed(text):
+    return f'embedding external by {text}'
+
 def embed_api(text):
     vec = embed(text)
     return {'vector': vec, 'dim': len(vec)}
