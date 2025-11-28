fc6e73614359302f1f284c3274a7bab84e0a7d7f
inspire12 <ox4443@naver.com>
2025-11-28 15:53:57 +0900

    feat: add embedding api

--- embedding.py
+++ embedding.py
@@ -3,6 +3,9 @@ def embedding():
     print("call gpt")
     print("save embedding")
 
+def embed(text):
+    return f'embedded {text}'
+
 def embed_api(text):
     vec = embed(text)
     return {'vector': vec, 'dim': len(vec)}
